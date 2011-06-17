0dc0e08f22c30252519fa89510906b0f692953e3
Eduardo Cobuci <educobuci@gmail.com>
2011-06-17 16:46:42 -0300

    calling validations on save

--- lib/citier/child_instance_methods.rb
+++ lib/citier/child_instance_methods.rb
@@ -47,7 +47,7 @@ module ChildInstanceMethods
       citier_debug("SQL : #{sql}")
       self.connection.execute(sql)
     end
-    return parent_saved && current_saved
+    return parent_saved && current_saved && self.valid?
   end
   
   def save!
