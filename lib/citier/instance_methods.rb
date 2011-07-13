54f1dd31aa8712da84dfe05126ee814e566debdd
Daniel Morgan <danmorgz@gmail.com>
2011-07-13 14:51:15 +0100

    Added a validator that can be used instead of the normal :uniqueness
    # USAGE validates :attribute, :citier_uniqueness => true
    
    Needed because validates :attribute, :uniqueness => true  Won't work because it tries to call child_class.attribute on parents table
    
    This is WRONG:
    SELECT  1 FROM `Facility` WHERE (`view_swimming_pools`.`tldc_id` = 1234)
    
    It needs to be either:
    SELECT  1 FROM `view_swimming_pools` WHERE (`view_swimming_pools`.`tldc_id` = 1234)
    
    Or
    
    SELECT  1 FROM `Facility` WHERE (`tldc_id` = 1234)

--- lib/citier/instance_methods.rb
+++ lib/citier/instance_methods.rb
@@ -26,5 +26,15 @@ module InstanceMethods
   def destroy
     return self.delete
   end
+  
+  # USAGE validates :attribute, :citier_uniqueness => true
+  # Needed because validates :attribute, :uniqueness => true  Won't work because it tries to call child_class.attribute on parents table
+  class CitierUniquenessValidator < ActiveModel::EachValidator  
+    def validate_each(object, attribute, value)
+      if object.class.where(attribute.to_sym => value).limit(1).first 
+            object.errors[attribute] << (options[:message] || "has already been taken.")  
+      end 
+    end  
+  end
 
 end
