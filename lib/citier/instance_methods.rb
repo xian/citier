6a76ce6fcc1089596c2e9e1eb3df04227b224803
Daniel Morgan <danmorgz@gmail.com>
2011-07-16 12:53:15 +0100

    Fixing up CitierUniquenessValidator to compare the child as root against the root

--- lib/citier/instance_methods.rb
+++ lib/citier/instance_methods.rb
@@ -57,7 +57,7 @@ module InstanceMethods
   class CitierUniquenessValidator < ActiveModel::EachValidator  
     def validate_each(object, attribute, value)
       existing_record = object.class.where(attribute.to_sym => value).limit(1).first
-      if existing_record && existing_record != object #if prev record exist and it isn't our current obj
+      if existing_record && existing_record.as_root != object.as_root #if prev record exist and it isn't our current obj
             object.errors[attribute] << (options[:message] || "has already been taken.")  
       end 
     end  
