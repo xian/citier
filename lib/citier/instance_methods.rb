296afc6400b4fc4e162c8d045632edf008ea0845
Christian Williams <xian@xian-laptop.local>
2011-09-22 17:50:18 -0700

    Fix

--- lib/citier/instance_methods.rb
+++ lib/citier/instance_methods.rb
@@ -13,14 +13,14 @@ module Citier
   
     # USAGE validates :attribute, :citier_uniqueness => true
     # Needed because validates :attribute, :uniqueness => true  Won't work because it tries to call child_class.attribute on parents table
-    class CitierUniquenessValidator < ActiveModel::EachValidator  
-      def validate_each(object, attribute, value)
-        existing_record = object.class.where(attribute.to_sym => value).limit(1).first
-        if existing_record && existing_record.as_root != object.as_root #if prev record exist and it isn't our current obj
-              object.errors[attribute] << (options[:message] || "has already been taken.")  
-        end 
-      end  
-    end
+    #class CitierUniquenessValidator < ActiveModel::EachValidator
+    #  def validate_each(object, attribute, value)
+    #    existing_record = object.class.where(attribute.to_sym => value).limit(1).first
+    #    if existing_record && existing_record.as_root != object.as_root #if prev record exist and it isn't our current obj
+    #          object.errors[attribute] << (options[:message] || "has already been taken.")
+    #    end
+    #  end
+    #end
 
   end
 end
