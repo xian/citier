3b95a7c65fe14c0e7199ef8e31d9e0e86a6c6ce9
Christian Williams <xian@squareup.com>
2011-10-08 10:10:28 -0700

    Syntax fix for Ruby 1.8.7.

--- lib/citier/core_ext.rb
+++ lib/citier/core_ext.rb
@@ -57,7 +57,7 @@ def create_citier_view(theclass)  #function for creating views for migrations
   sql = "CREATE VIEW #{self_read_table} AS SELECT #{parent_read_table}.id, #{columns.join(',')} FROM #{parent_read_table}, #{self_write_table} WHERE #{parent_read_table}.id = #{self_write_table}.id" 
   
   #Use our rails_sql_views gem to create the view so we get it outputted to schema
-  create_view "#{self_read_table}", "SELECT #{parent_read_table}.id, #{columns.join(',')} FROM #{parent_read_table}, #{self_write_table} WHERE #{parent_read_table}.id = #{self_write_table}.id", do |v|
+  create_view "#{self_read_table}", "SELECT #{parent_read_table}.id, #{columns.join(',')} FROM #{parent_read_table}, #{self_write_table} WHERE #{parent_read_table}.id = #{self_write_table}.id" do |v|
     v.column :id
     columns.each do |c|
       v.column c.to_sym
